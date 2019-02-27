7dac7ae6c0af1aa1732ff598858cfe4fb44f6b86
matogolf <matogolf@hotmail.com>
2019-02-27 17:16:00 +0100

    LICENSE added

--- popup.js
+++ popup.js
@@ -1,8 +1,30 @@
+//
+//  JavaScript Restrictor is a browser extension which increases level
+//  of security, anonymity and privacy of the user while browsing the
+//  internet.
+//
+//  Copyright (C) 2019  Martin Timko
+//  Copyright (C) 2019  Libor Polcak
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+
 if ((typeof chrome) !== "undefined") {
   var browser = chrome;
 }
 
-// var bkgp = browser.extension.getBackgroundPage();
 const L0 = 0;
 const L1 = 1;
 const L2 = 2;
@@ -14,7 +36,7 @@ const fadeOut = "0.3";
 const fadeIn = "1.0";
 
 var myAddon = new URL(browser.runtime.getURL ('./'));
-var url; // povodne domain "www.example.com"
+var url; // "www.example.com"
 var activeClass = "level active";
 var activeLevel;
 var subDomain = false;
@@ -154,11 +176,11 @@ function clearAllLevels(def) {
 }
 
 
+//find url of current tab where popup showed
 var queryInfo = {
   active: true,
   currentWindow: true
 };
-//find url of current tab where popup showed
 browser.tabs.query(queryInfo, function(tabs) {
 
     let tab = tabs[0];
