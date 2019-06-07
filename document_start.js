d6336bd92a26ccdd1270eedcfd21acaac570cad0
Libor Polčák <ipolcak@fit.vutbr.cz>
2019-06-07 16:39:31 +0200

    content script: create URL module
    Improve code readability

--- document_start.js
+++ document_start.js
@@ -575,21 +575,3 @@ function createDNTWrappingFunctionString(selectOption) {
 }
 
 
-
-function extractRootDomain(thisDomain) {
-    // var thisDomain = extractHostname(thisUrl);
-    var splitArr = thisDomain.split('.');
-    var arrLen = splitArr.length;
-    //extracting the root domain here
-    //if there is a subdomain 
-    if (arrLen > 2) {
-        thisDomain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
-        //check to see if it's using a Country Code Top Level Domain (ccTLD) (e.g. ".co.uk")
-        if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2) {
-            //this is using a ccTLD
-            thisDomain = splitArr[arrLen - 3] + '.' + thisDomain;
-        }
-    }
-    return thisDomain;
-}
-
