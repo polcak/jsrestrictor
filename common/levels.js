e10e73bc815783cceb8760da2dd7fc25e2045737
peter.hornak <peter.hornak@kiwi.com>
2020-04-10 15:57:16 +0200

    Fix level changing

--- common/levels.js
+++ common/levels.js
@@ -23,84 +23,91 @@
 // levels of protection
 
 var level_0 = {
-	"level_id": "0",
-	"level_text": "Built-in 0",
-	"level_description": "No protection at all",
-	"wrappers": [],
-}
+    "level_id": "0",
+    "level_text": "Built-in 0",
+    "level_description": "No protection at all",
+    "wrappers": [],
+};
 
 var level_1_time_precision = 2;
 var level_1 = {
-	"level_id": "1",
-	"level_text": "Built-in 1",
-	"level_description": "Minimal level of protection",
-	"wrappers": [
-		// HRT
-		["Performance.prototype.now", level_1_time_precision],
-		// PT2
-		["performance.getEntries", level_1_time_precision],
-		["performance.getEntriesByName", level_1_time_precision],
-		["performance.getEntriesByType", level_1_time_precision],
-		// ECMA
-		["window.Date", level_1_time_precision],
-		// DM
-		["navigator.deviceMemory"],
-		// HTML-LS
-		["navigator.hardwareConcurrency"],
-	]
-}
+    "level_id": "1",
+    "level_text": "Built-in 1",
+    "level_description": "Minimal level of protection",
+    "wrappers": [
+        // HRT
+        ["Performance.prototype.now", level_1_time_precision],
+        // PT2
+        ["performance.getEntries", level_1_time_precision],
+        ["performance.getEntriesByName", level_1_time_precision],
+        ["performance.getEntriesByType", level_1_time_precision],
+        // ECMA
+        ["window.Date", level_1_time_precision],
+        // DM
+        ["navigator.deviceMemory"],
+        // HTML-LS
+        ["navigator.hardwareConcurrency"],
+    ]
+};
 
 var level_2_time_precision = 1;
 var level_2 = {
-	"level_id": "2",
-	"level_text": "Built-in 2",
-	"level_description": "Recomended level of protection for most sites",
-	"wrappers": [
-		// H-C
-		["CanvasRenderingContext2D.prototype.getImageData"],
-		["HTMLCanvasElement.prototype.toBlob"],
-		["HTMLCanvasElement.prototype.toDataURL"],
-		// HRT
-		["Performance.prototype.now", level_2_time_precision],
-		// PT2
-		["performance.getEntries", level_2_time_precision],
-		["performance.getEntriesByName", level_2_time_precision],
-		["performance.getEntriesByType", level_2_time_precision],
-		// ECMA
-		["window.Date", level_2_time_precision],
-		// DM
-		["navigator.deviceMemory"],
-		// HTML-LS
-		["navigator.hardwareConcurrency"],
-	]
-}
+    "level_id": "2",
+    "level_text": "Built-in 2",
+    "level_description": "Recomended level of protection for most sites",
+    "wrappers": [
+        // H-C
+        ["CanvasRenderingContext2D.prototype.getImageData"],
+        ["HTMLCanvasElement.prototype.toBlob"],
+        ["HTMLCanvasElement.prototype.toDataURL"],
+        // HRT
+        ["Performance.prototype.now", level_2_time_precision],
+        // PT2
+        ["performance.getEntries", level_2_time_precision],
+        ["performance.getEntriesByName", level_2_time_precision],
+        ["performance.getEntriesByType", level_2_time_precision],
+        // ECMA
+        ["window.Date", level_2_time_precision],
+        // DM
+        ["navigator.deviceMemory"],
+        // HTML-LS
+        ["navigator.hardwareConcurrency"],
+    ]
+};
 
 var level_3_time_precision = 0;
 var level_3 = {
-	"level_id": "3",
-	"level_text": "Built-in 3",
-	"level_description": "High level of protection",
-	"wrappers": [
-		// H-C
-		["CanvasRenderingContext2D.prototype.getImageData"],
-		["HTMLCanvasElement.prototype.toBlob"],
-		["HTMLCanvasElement.prototype.toDataURL"],
-		// HRT
-		["Performance.prototype.now", level_3_time_precision],
-		// PT2
-		["performance.getEntries", level_3_time_precision],
-		["performance.getEntriesByName", level_3_time_precision],
-		["performance.getEntriesByType", level_3_time_precision],
-		// ECMA
-		["window.Date", level_3_time_precision],
-		// AJAX
-		["window.XMLHttpRequest", false, true],
-		// DM
-		["navigator.deviceMemory"],
-		// HTML-LS
-		["navigator.hardwareConcurrency"],
-	]
-}
+    "level_id": "3",
+    "level_text": "Built-in 3",
+    "level_description": "High level of protection",
+    "wrappers": [
+        // H-C
+        ["CanvasRenderingContext2D.prototype.getImageData"],
+        ["HTMLCanvasElement.prototype.toBlob"],
+        ["HTMLCanvasElement.prototype.toDataURL"],
+        // HRT
+        ["Performance.prototype.now", level_3_time_precision],
+        // PT2
+        ["performance.getEntries", level_3_time_precision],
+        ["performance.getEntriesByName", level_3_time_precision],
+        ["performance.getEntriesByType", level_3_time_precision],
+        // ECMA
+        ["window.Date", level_3_time_precision],
+        // AJAX
+        ["window.XMLHttpRequest", false, true],
+        // DM
+        ["navigator.deviceMemory"],
+        // HTML-LS
+        ["navigator.hardwareConcurrency"],
+    ]
+};
+
+var level_4 = {
+    "level_id": "4",
+    "level_text": "Built-in 4",
+    "level_description": "Chrome Zero porting",
+    "wrappers": []
+};
 
 // default extension_settings_data setting. used on install
 var extension_settings_data = level_0;
@@ -110,68 +117,75 @@ const L0 = "0";
 const L1 = "1";
 const L2 = "2";
 const L3 = "3";
+const L4 = "4";
 
-var levels = {}
+var levels = {};
 var default_level = {};
 var domains = {};
+
 function init_levels() {
-	levels = {
-		[level_0.level_id]: level_0,
-		[level_1.level_id]: level_1,
-		[level_2.level_id]: level_2,
-		[level_3.level_id]: level_3
-	};
-	default_level = Object.create(levels[L2]);
-	default_level.level_text = "Default";
-	domains = {};
+    levels = {
+        [level_0.level_id]: level_0,
+        [level_1.level_id]: level_1,
+        [level_2.level_id]: level_2,
+        [level_3.level_id]: level_3,
+        [level_4.level_id]: level_4
+    };
+    default_level = Object.create(levels[L4]);
+    default_level.level_text = "Default";
+    domains = {};
 }
+
 init_levels();
 
 let levels_initialised = false;
 let levels_updated_callbacks = [];
+
 function updateLevels(res) {
-	init_levels();
-	custom_levels = res["custom_levels"] || {};
-	for (let key in custom_levels) {
-		levels[custom_levels[key].level_id] = custom_levels[key];
-	}
-	var new_default_level = res["__default__"];
-	if (new_default_level === undefined || new_default_level === null || !(new_default_level in levels)) {
-		default_level = Object.create(levels[L2]);
-		setDefaultLevel(L2);
-	}
-	else {
-		default_level = Object.create(levels[new_default_level]);
-	}
-	default_level.is_default = true;
-	var new_domains = res["domains"] || {};
-	for (let d in new_domains) {
-		domains[d] = new_domains[d];
-	}
-	var orig_levels_updated_callbacks = levels_updated_callbacks;
-	levels_updated_callbacks = [];
-	orig_levels_updated_callbacks.forEach((it) => it());
-	levels_initialised = true;
+    init_levels();
+    custom_levels = res["custom_levels"] || {};
+    for (let key in custom_levels) {
+        levels[custom_levels[key].level_id] = custom_levels[key];
+    }
+    var new_default_level = res["__default__"];
+    if (new_default_level === undefined || new_default_level === null || !(new_default_level in levels)) {
+        default_level = Object.create(levels[L2]);
+        setDefaultLevel(L2);
+    } else {
+        default_level = Object.create(levels[new_default_level]);
+    }
+    default_level.is_default = true;
+    var new_domains = res["domains"] || {};
+    for (let d in new_domains) {
+        domains[d] = new_domains[d];
+    }
+    var orig_levels_updated_callbacks = levels_updated_callbacks;
+    levels_updated_callbacks = [];
+    orig_levels_updated_callbacks.forEach((it) => it());
+    levels_initialised = true;
 }
+
 browser.storage.sync.get(null, updateLevels);
 
 function changedLevels(changed, area) {
-	browser.storage.sync.get(null, updateLevels);
+    browser.storage.sync.get(null, updateLevels);
 }
+
 browser.storage.onChanged.addListener(changedLevels);
 
 function setDefaultLevel(level) {
-	browser.storage.sync.set({__default__: level});
+    browser.storage.sync.set({__default__: level});
 }
 
 function saveDomainLevels() {
-	browser.storage.sync.set({domains: domains});
+    browser.storage.sync.set({domains: domains});
 }
 
 function getCurrentLevelJSON(url) {
-	var rootDomain = extractRootDomain(new URL(url).hostname.replace(/^www\./,''));
-	if (rootDomain in domains) {
-		return levels[domains[rootDomain].level_id];
-	}
-	return default_level;
+    var subDomains = extractSubDomains(new URL(url).hostname.replace(/^www\./, ''));
+    for (let domain of subDomains.reverse()) {
+        if (domain in domains)
+            return levels[domains[domain].level_id];
+    }
+    return default_level;
 }
