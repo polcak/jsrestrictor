c4a328c44f9a7b93f0709dc886eea7ef4fee33f5
martinbednar <xbedna60@fit.vutbr.cz>
2020-05-09 12:30:17 +0200

    Test first null values in gps testing.

--- tests/integration_tests/tests_definition/test_gps.py
+++ tests/integration_tests/tests_definition/test_gps.py
@@ -16,17 +16,36 @@ def position(browser):
 ## Test accuracy of the latitude and longitude properties in meters.
 def test_accuracy(browser, position, expected):
     if expected.geolocation.accuracy['value'] == 'REAL VALUE':
-        if expected.geolocation.accuracy['accuracy'] == 'EXACTLY':
-            if position['accuracy'] == "null":
-                # If current value is null, real value has to be null too.
-                assert position['accuracy'] == browser.real.geolocation.accuracy
-            else:
+        if position['accuracy'] == "null":
+            # If current value is null, real value has to be null too.
+            assert position['accuracy'] == browser.real.geolocation.accuracy
+        else:
+            if expected.geolocation.accuracy['accuracy'] == 'EXACTLY':
                 # Values do not have to be strictly equal.
                 # A deviation of less than 50 meters is tolerated.
                 assert abs(float(position['accuracy']) - float(browser.real.geolocation.accuracy)) < 50
+            else:
+                # Should be rounded real value in accuracy.
+                assert is_in_accuracy(position['accuracy'], expected.geolocation.accuracy['accuracy'])
+    else:
+        # Should be spoofed value.
+        assert position['accuracy'] == expected.geolocation.accuracy['value']
+
+
+## Test accuracy of the latitude and longitude properties in meters.
+def test_accuracy(browser, position, expected):
+    if expected.geolocation.accuracy['value'] == 'REAL VALUE':
+        if position['accuracy'] == "null":
+            # If current value is null, real value has to be null too.
+            assert position['accuracy'] == browser.real.geolocation.accuracy
         else:
-            # Should be rounded real value in accuracy.
-            assert is_in_accuracy(position['accuracy'], expected.geolocation.accuracy['accuracy'])
+            if expected.geolocation.accuracy['accuracy'] == 'EXACTLY':
+                # Values do not have to be strictly equal.
+                # A deviation of less than 50 meters is tolerated.
+                assert abs(float(position['accuracy']) - float(browser.real.geolocation.accuracy)) < 50
+            else:
+                # Should be rounded real value in accuracy.
+                assert is_in_accuracy(position['accuracy'], expected.geolocation.accuracy['accuracy'])
     else:
         # Should be spoofed value.
         assert position['accuracy'] == expected.geolocation.accuracy['value']
@@ -35,17 +54,17 @@ def test_accuracy(browser, position, expected):
 ## Test position's altitude in meters, relative to sea level.
 def test_altitude(browser, position, expected):
     if expected.geolocation.altitude['value'] == 'REAL VALUE':
-        if expected.geolocation.altitude['accuracy'] == 'EXACTLY':
-            if position['altitude'] == "null":
-                # If current value is null, real value has to be null too.
-                assert position['altitude'] == browser.real.geolocation.altitude
-            else:
+        if position['altitude'] == "null":
+            # If current value is null, real value has to be null too.
+            assert position['altitude'] == browser.real.geolocation.altitude
+        else:
+            if expected.geolocation.altitude['accuracy'] == 'EXACTLY':
                 # Values do not have to be strictly equal.
                 # A deviation of less than 10 meters is tolerated.
                 assert abs(float(position['altitude']) - float(browser.real.geolocation.altitude)) < 10
-        else:
-            # Should be rounded real value in accuracy.
-            assert is_in_accuracy(position['altitude'], expected.geolocation.altitude['accuracy'])
+            else:
+                # Should be rounded real value in accuracy.
+                assert is_in_accuracy(position['altitude'], expected.geolocation.altitude['accuracy'])
     else:
         # Should be spoofed value.
         assert position['altitude'] == expected.geolocation.altitude['value']
@@ -54,18 +73,18 @@ def test_altitude(browser, position, expected):
 ## Test accuracy of the altitude property in meters.
 def test_altitudeaccurac(browser, position, expected):
     if expected.geolocation.altitudeAccurac['value'] == 'REAL VALUE':
-        if expected.geolocation.altitudeAccurac['accuracy'] == 'EXACTLY':
-            if position['altitudeaccurac'] == "null":
+        if position['altitudeaccurac'] == "null":
                 # If current value is null, real value has to be null too.
                 assert position['altitudeaccurac'] == browser.real.geolocation.altitudeAccurac
-            else:
+        else:
+            if expected.geolocation.altitudeAccurac['accuracy'] == 'EXACTLY':
                 # Values do not have to be strictly equal.
                 # A deviation of less than 10 meters is tolerated.
                 assert abs(float(position['altitudeaccurac']) - float(browser.real.geolocation.altitudeAccurac)) < 10
-        else:
-            # Should be rounded real value in accuracy.
-            assert is_in_accuracy(position['altitudeaccurac'],
-                                  expected.geolocation.altitudeAccurac['accuracy'])
+            else:
+                # Should be rounded real value in accuracy.
+                assert is_in_accuracy(position['altitudeaccurac'],
+                                      expected.geolocation.altitudeAccurac['accuracy'])
     else:
         # Should be spoofed value.
         assert position['altitudeaccurac'] == expected.geolocation.altitudeAccurac['value']
@@ -79,17 +98,17 @@ def test_altitudeaccurac(browser, position, expected):
 # If speed is 0, heading is NaN. If the device is unable to provide heading information, this value is null
 def test_heading(browser, position, expected):
     if expected.geolocation.heading['value'] == 'REAL VALUE':
-        if expected.geolocation.heading['accuracy'] == 'EXACTLY':
-            if position['heading'] == "null":
-                # If current value is null, real value has to be null too.
-                assert position['heading'] == browser.real.geolocation.heading
-            else:
+        if position['heading'] == "null":
+            # If current value is null, real value has to be null too.
+            assert position['heading'] == browser.real.geolocation.heading
+        else:
+            if expected.geolocation.heading['accuracy'] == 'EXACTLY':
                 # Values do not have to be strictly equal.
                 # A deviation of less than 30 degrees is tolerated.
                 assert abs(float(position['heading']) - float(browser.real.geolocation.heading)) < 30
-        else:
-            # Should be rounded real value in accuracy.
-            assert is_in_accuracy(position['heading'], expected.geolocation.heading['accuracy'])
+            else:
+                # Should be rounded real value in accuracy.
+                assert is_in_accuracy(position['heading'], expected.geolocation.heading['accuracy'])
     else:
         # Should be spoofed value.
         assert position['heading'] == expected.geolocation.heading['value']
@@ -98,18 +117,18 @@ def test_heading(browser, position, expected):
 ## Test position's latitude in decimal degrees.
 def test_latitude(browser, position, expected):
     if expected.geolocation.latitude['value'] == 'REAL VALUE':
-        if expected.geolocation.latitude['accuracy'] == 'EXACTLY':
-            if position['latitude'] == "null":
-                # If current value is null, real value has to be null too.
-                assert position['latitude'] == browser.real.geolocation.latitude
-            else:
+        if position['latitude'] == "null":
+            # If current value is null, real value has to be null too.
+            assert position['latitude'] == browser.real.geolocation.latitude
+        else:
+            if expected.geolocation.latitude['accuracy'] == 'EXACTLY':
                 # Values do not have to be strictly equal.
                 # A deviation of less than 1 degrees is tolerated.
                 assert abs(float(position['latitude']) - float(browser.real.geolocation.latitude)) < 1
-        else:
-            # Should be rounded real value in accuracy.
-            assert is_in_accuracy(round(float(position['latitude']), 3) * 1000,
-                                  expected.geolocation.latitude['accuracy'] * 1000)
+            else:
+                # Should be rounded real value in accuracy.
+                assert is_in_accuracy(round(float(position['latitude']), 3) * 1000,
+                                      expected.geolocation.latitude['accuracy'] * 1000)
     else:
         # Should be spoofed value.
         assert position['latitude'] == expected.geolocation.latitude['value']
@@ -118,18 +137,18 @@ def test_latitude(browser, position, expected):
 ## Test position's longitude in decimal degrees.
 def test_longitude(browser, position, expected):
     if expected.geolocation.longitude['value'] == 'REAL VALUE':
-        if expected.geolocation.longitude['accuracy'] == 'EXACTLY':
-            if position['longitude'] == "null":
-                # If current value is null, real value has to be null too.
-                assert position['longitude'] == browser.real.geolocation.longitude
-            else:
+        if position['longitude'] == "null":
+            # If current value is null, real value has to be null too.
+            assert position['longitude'] == browser.real.geolocation.longitude
+        else:
+            if expected.geolocation.longitude['accuracy'] == 'EXACTLY':
                 # Values do not have to be strictly equal.
                 # A deviation of less than 1 degrees is tolerated.
                 assert abs(float(position['longitude']) - float(browser.real.geolocation.longitude)) < 1
-        else:
-            # Should be rounded real value in accuracy.
-            assert is_in_accuracy(round(float(position['longitude']), 3) * 1000,
-                                  expected.geolocation.longitude['accuracy'] * 1000)
+            else:
+                # Should be rounded real value in accuracy.
+                assert is_in_accuracy(round(float(position['longitude']), 3) * 1000,
+                                      expected.geolocation.longitude['accuracy'] * 1000)
     else:
         # Should be spoofed value.
         assert position['longitude'] == expected.geolocation.longitude['value']
@@ -138,17 +157,17 @@ def test_longitude(browser, position, expected):
 ## Test speed (velocity) of the device in meters per second. This value can be null.
 def test_speed(browser, position, expected):
     if expected.geolocation.speed['value'] == 'REAL VALUE':
-        if expected.geolocation.speed['accuracy'] == 'EXACTLY':
-            if position['speed'] == "null":
-                # If current value is null, real value has to be null too.
-                assert position['speed'] == browser.real.geolocation.speed
-            else:
+        if position['speed'] == "null":
+            # If current value is null, real value has to be null too.
+            assert position['speed'] == browser.real.geolocation.speed
+        else:
+            if expected.geolocation.speed['accuracy'] == 'EXACTLY':
                 # Values do not have to be strictly equal.
                 # A deviation of less than 5 meters per second is tolerated.
                 assert abs(float(position['speed']) - float(browser.real.geolocation.speed)) < 5
-        else:
-            # Should be rounded real value in accuracy.
-            assert is_in_accuracy(position['speed'], expected.geolocation.speed['accuracy'])
+            else:
+                # Should be rounded real value in accuracy.
+                assert is_in_accuracy(position['speed'], expected.geolocation.speed['accuracy'])
     else:
         # Should be spoofed value.
         assert position['speed'] == expected.geolocation.speed['value']
