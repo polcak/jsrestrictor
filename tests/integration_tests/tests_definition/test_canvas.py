7a017d14d61014feb889526f5c9a39bdd3d437be
martinbednar <xbedna60@fit.vutbr.cz>
2020-05-20 15:18:40 +0200

    Add expected failure to canvas test
    explained in issue #23.

--- tests/integration_tests/tests_definition/test_canvas.py
+++ tests/integration_tests/tests_definition/test_canvas.py
@@ -1,7 +1,11 @@
+import pytest
+
 from values_getters import is_canvas_spoofed
 
 
 ## Test canvas - if canvas is spoofed: Reading from canvas returns white image.
+## Expected failure - look at: Issue #23.
+@pytest.mark.xfail
 def test_canvas(browser, expected):
     if expected.protect_canvas:
         assert is_canvas_spoofed(browser.driver)
