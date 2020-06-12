fc25314e3c68c052794739b82220f95c0c1ee61f
Libor Polčák <ipolcak@fit.vutbr.cz>
2020-06-12 15:08:17 +0200

    Remove window.PerformanceEntry wrapping
    In reality it was PerformanceEntry.startTime and
    PerformanceEntry.duration wrapping, see #57.
    
    Testing page https://www.fit.vutbr.cz/~ipolcak/firefox_csp_bug/nocsp.php
    is not influenced by the wrapping. It is unclear when is the wrapping
    applied.

--- common/wrappingS-HRT.js
+++ common/wrappingS-HRT.js
@@ -24,20 +24,6 @@
  * Create private namespace
  */
 (function() {
-	function changePropertyPrototype(name) {
-		let descriptor = Object.getOwnPropertyDescriptor(PerformanceEntry.prototype, name);
-		let originalF = descriptor['get'];
-		let replacementF = function() {
-			let originalVal = originalF.call(this, ...arguments);
-			return func(originalVal, precision);
-			// Replace this when injecting, to differ between startTime and duration functions
-			'__name__';
-		};
-		descriptor['get'] = replacementF;
-		original_functions[replacementF.toString()] = originalF.toString();
-		Object.defineProperty(PerformanceEntry.prototype, name, descriptor);
-	}
-
 	var wrappers = [
 		{
 			parent_object: "Performance.prototype",
@@ -68,21 +54,6 @@
 					return func(originalPerformanceValue, precision);
 				`
 		},
-		{
-			parent_object: "window",
-			parent_object_property: "PerformanceEntry",
-			wrapped_objects: [],
-			helping_code: rounding_function + noise_function + `
-			let precision = args[0];
-			let doNoise = args[1];
-			var func = rounding_function;
-			if (doNoise === true){
-				func = noise_function
-			}
-			(${changePropertyPrototype.toString().split("__name__").join('"startTime"')})('startTime');
-			(${changePropertyPrototype.toString().split("__name__").join('"duration"')})('duration');
-			`
-		}
 	];
 	add_wrappers(wrappers);
 })();
