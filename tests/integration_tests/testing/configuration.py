885b4d79abe2130dbc5fd11253b6c9790a9e7ae7
martinbednar <martin.bedn20@gmail.com>
2020-07-29 09:14:14 +0200

    Add integration tests.

---
+++ tests/integration_tests/testing/configuration.py
@@ -0,0 +1,66 @@
+﻿#
+#  JavaScript Restrictor is a browser extension which increases level
+#  of security, anonymity and privacy of the user while browsing the
+#  internet.
+#
+#  Copyright (C) 2020  Martin Bednar
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from pathlib import Path
+
+from web_browser_type import BrowserType
+
+
+## Static Config class contains definition for basic variables used during testing.
+#
+#  Class Config is automatically changed for every computer based on current environment.
+#  Variables in brackets <<>> are updated by script setup_buildJSR_runTests.ps1/sh.
+#  You can change testing browsers and JSR levels.
+class __Config:
+    # Browsers in which tests will be run.
+    tested_browsers = [BrowserType.CHROME, BrowserType.FIREFOX]
+    # Default levels of JSR which will be tested.
+    tested_jsr_levels = [0, 1, 2, 3]
+
+
+
+    ######### OTHER CONFIG PARAMETERS SHOULD BE SET AUTOMATICALLY BY SCRIPT setup_buildJSR_runTests.ps1/sh ########
+    # IT SHOULD NOT BE NEEDED TO CHANGE THEM, BUT WHEN ERROR HAPPENED, TRY TO INSERT ABSOLUTE FULL PATHS MANUALLY #
+    # Full path to Firefox driver.
+    firefox_driver = "<<JSR_project_root_directory_path>>/tests/common_files/webbrowser_drivers/geckodriver.exe"
+    # Full path to JSR package for Firefox (xpi package).
+    firefox_jsr_extension = "<<JSR_project_root_directory_path>>/tests/common_files/JSR/firefox_JSR.xpi"
+    # Full path to Firefox ESR (default) profile.
+    firefox_profile = "<<Firefox_ESR_default_profile>>"
+    # Full path to Chrome driver.
+    chrome_driver = "<<JSR_project_root_directory_path>>/tests/common_files/webbrowser_drivers/chromedriver.exe"
+    # Full path to JSR package for Chrome (crx package).
+    chrome_jsr_extension = "<<JSR_project_root_directory_path>>/tests/common_files/JSR/chrome_JSR.crx"
+    # Support testing page - do not change without changing script values_getters.py
+    # DO NOT SET CUSTOM level of protection FOR THIS SITE. DEFAULT LEVEL TO THIS SITE HAS TO BE APPLIED:
+    testing_page = "https://polcak.github.io/jsrestrictor/test/test.html"
+
+
+## Getter for values in Config class
+#
+#  Do not use direct access to Config from other modules. Always get values through this getter.
+#  It is because of paths. This getter guarantees uniform way of writing paths in Config class for every supported OS.
+#  Before getting a path, this path is updated in this getter according current OS.
+def get_config(item):
+    if item == "tested_browsers" or item == "tested_jsr_levels" or item == "testing_page":
+        return getattr(__Config, item)
+    else:
+        return str(Path(getattr(__Config, item)))
---
+++ tests/integration_tests/testing/values_expected.py
@@ -0,0 +1,193 @@
+#
+#  JavaScript Restrictor is a browser extension which increases level
+#  of security, anonymity and privacy of the user while browsing the
+#  internet.
+#
+#  Copyright (C) 2020  Martin Bednar
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from values_tested import TestedValues
+from web_browser_type import BrowserType
+
+
+## Module contains definitions for expected values of default levels od JSR.
+#
+#  Expected values are comparing during testing with current values of variables.
+#  'REAL VALUE' means that current value should not be spoofed.
+#  'EXACTLY' means that current value should not be rounded
+#       (the small deviation caused by the script runtime is neglected)
+#  This module can be edited when definition of default levels will be changed.
+
+
+## Expected values for default level 0 of JSR.
+level0 = TestedValues(
+    user_agent={BrowserType.FIREFOX: 'REAL VALUE',
+                BrowserType.CHROME: 'REAL VALUE'},
+    app_version='REAL VALUE',
+    platform='REAL VALUE',
+    vendor={BrowserType.FIREFOX: 'REAL VALUE',
+            BrowserType.CHROME: 'REAL VALUE'},
+    language='REAL VALUE',
+    languages='REAL VALUE',
+    do_not_track='REAL VALUE',
+    cookie_enabled='REAL VALUE',
+    oscpu='REAL VALUE',
+    gps_accuracy={'value': 'REAL VALUE',
+                  'accuracy': 'EXACTLY'},
+    altitude={'value': 'REAL VALUE',
+              'accuracy': 'EXACTLY'},
+    altitude_accurac={'value': 'REAL VALUE',
+                      'accuracy': 'EXACTLY'},
+    heading={'value': 'REAL VALUE',
+             'accuracy': 'EXACTLY'},
+    latitude={'value': 'REAL VALUE',
+              'accuracy': 'EXACTLY'},
+    longitude={'value': 'REAL VALUE',
+               'accuracy': 'EXACTLY'},
+    speed={'value': 'REAL VALUE',
+           'accuracy': 'EXACTLY'},
+    timestamp={'value': 'REAL VALUE',
+               'accuracy': 'EXACTLY'},
+    device_memory={BrowserType.FIREFOX: 'REAL VALUE',
+                   BrowserType.CHROME: 'REAL VALUE'},
+    hardware_concurrency='REAL VALUE',
+    referrer='REAL VALUE',
+    time={'value': 'REAL VALUE',
+          'accuracy': 'EXACTLY'},
+    performance={'value': 'REAL VALUE',
+                 'accuracy': 'EXACTLY'},
+    protect_canvas=False
+)
+
+## Expected values for default level 1 of JSR.
+level1 = TestedValues(
+    user_agent={BrowserType.FIREFOX: 'REAL VALUE',
+                BrowserType.CHROME: 'REAL VALUE'},
+    app_version='REAL VALUE',
+    platform='REAL VALUE',
+    vendor={BrowserType.FIREFOX: 'REAL VALUE',
+            BrowserType.CHROME: 'REAL VALUE'},
+    language='REAL VALUE',
+    languages='REAL VALUE',
+    do_not_track='REAL VALUE',
+    cookie_enabled='REAL VALUE',
+    oscpu='REAL VALUE',
+    gps_accuracy={'value': 'REAL VALUE',
+                  'accuracy': 10},
+    altitude={'value': 'REAL VALUE',
+              'accuracy': 10},
+    altitude_accurac={'value': 'REAL VALUE',
+                      'accuracy': 10},
+    heading={'value': 'REAL VALUE',
+             'accuracy': 10},
+    latitude={'value': 'REAL VALUE',
+              'accuracy': 0.01},
+    longitude={'value': 'REAL VALUE',
+               'accuracy': 0.01},
+    speed={'value': 'REAL VALUE',
+           'accuracy': 10},
+    timestamp={'value': 'REAL VALUE',
+               'accuracy': 0.01},
+    device_memory={BrowserType.FIREFOX: None,
+                   BrowserType.CHROME: 4},
+    hardware_concurrency=2,
+    referrer='REAL VALUE',
+    time={'value': 'REAL VALUE',
+          'accuracy': 0.01},
+    performance={'value': 'REAL VALUE',
+                 'accuracy': 10},
+    protect_canvas=False
+)
+
+## Expected values for default level 2 of JSR.
+level2 = TestedValues(
+    user_agent={BrowserType.FIREFOX: 'REAL VALUE',
+                BrowserType.CHROME: 'REAL VALUE'},
+    app_version='REAL VALUE',
+    platform='REAL VALUE',
+    vendor={BrowserType.FIREFOX: 'REAL VALUE',
+            BrowserType.CHROME: 'REAL VALUE'},
+    language='REAL VALUE',
+    languages='REAL VALUE',
+    do_not_track='REAL VALUE',
+    cookie_enabled='REAL VALUE',
+    oscpu='REAL VALUE',
+    gps_accuracy={'value': 'REAL VALUE',
+                  'accuracy': 100},
+    altitude={'value': 'REAL VALUE',
+              'accuracy': 100},
+    altitude_accurac={'value': 'REAL VALUE',
+                      'accuracy': 100},
+    heading={'value': 'REAL VALUE',
+             'accuracy': 100},
+    latitude={'value': 'REAL VALUE',
+              'accuracy': 0.1},
+    longitude={'value': 'REAL VALUE',
+               'accuracy': 0.1},
+    speed={'value': 'REAL VALUE',
+           'accuracy': 100},
+    timestamp={'value': 'REAL VALUE',
+               'accuracy': 0.1},
+    device_memory={BrowserType.FIREFOX: None,
+                   BrowserType.CHROME: 4},
+    hardware_concurrency=2,
+    referrer='REAL VALUE',
+    time={'value': 'REAL VALUE',
+          'accuracy': 0.1},
+    performance={'value': 'REAL VALUE',
+                 'accuracy': 100},
+    protect_canvas=True
+)
+
+## Expected values for default level 3 of JSR.
+level3 = TestedValues(
+    user_agent={BrowserType.FIREFOX: 'REAL VALUE',
+                BrowserType.CHROME: 'REAL VALUE'},
+    app_version='REAL VALUE',
+    platform='REAL VALUE',
+    vendor={BrowserType.FIREFOX: 'REAL VALUE',
+            BrowserType.CHROME: 'REAL VALUE'},
+    language='REAL VALUE',
+    languages='REAL VALUE',
+    do_not_track='REAL VALUE',
+    cookie_enabled='REAL VALUE',
+    oscpu='REAL VALUE',
+    gps_accuracy={'value': '0',
+                  'accuracy': 'EXACTLY'},
+    altitude={'value': '0',
+              'accuracy': 'EXACTLY'},
+    altitude_accurac={'value': '0',
+                      'accuracy': 'EXACTLY'},
+    heading={'value': 'REAL VALUE',
+             'accuracy': 100},
+    latitude={'value': 'REAL VALUE',
+              'accuracy': 0.1},
+    longitude={'value': 'REAL VALUE',
+               'accuracy': 0.1},
+    speed={'value': 'REAL VALUE',
+           'accuracy': 100},
+    timestamp={'value': '0',
+               'accuracy': 'EXACTLY'},
+    device_memory={BrowserType.FIREFOX: None,
+                   BrowserType.CHROME: 4},
+    hardware_concurrency=2,
+    referrer='REAL VALUE',
+    time={'value': 'REAL VALUE',
+          'accuracy': 1.0},
+    performance={'value': 'REAL VALUE',
+                 'accuracy': 1000},
+    protect_canvas=True
+)
