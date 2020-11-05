12a634b9e986d94609ae23acaf5a0091af270267
Libor Polčák <ipolcak@fit.vutbr.cz>
2020-11-05 16:23:10 +0100

    S-GEO: Adding missing header

--- common/wrappingS-GEO.js
+++ common/wrappingS-GEO.js
@@ -1,4 +1,25 @@
-
+//
+//  JavaScript Restrictor is a browser extension which increases level
+//  of security, anonymity and privacy of the user while browsing the
+//  internet.
+//
+//  Copyright (C) 2020  Libor Polcak
+//  Copyright (C) 2020  Peter Marko
+//  Copyright (C) 2019  Martin Timko
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
 
 (function() {
 	var processOriginalGPSDataObject = `
