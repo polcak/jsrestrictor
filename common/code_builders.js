3c5f786a55fbcdcdebae0f9e289c7e9d063c3e74
Libor Polčák <ipolcak@fit.vutbr.cz>
2020-11-25 11:11:17 +0100

    code_builders: Add possibility to disable calling Object.freeze
    You should be sure that this is a correct approach.
    To be used for navigator.geolocation wrapping.

--- common/code_builders.js
+++ common/code_builders.js
@@ -170,7 +170,11 @@ var build_code = function(wrapper, ...args) {
 				${wrapper.wrapper_prototype});
 		`
 	}
-	code += `Object.freeze(${wrapper.parent_object}.${wrapper.parent_object_property});`;
+	code += `
+		if (!${wrapper.nofreeze}) {
+			Object.freeze(${wrapper.parent_object}.${wrapper.parent_object_property});
+		}
+	`;
 	return enclose_wrapping(code, ...args);
 };
 
