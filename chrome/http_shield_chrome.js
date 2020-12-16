ffe51ceeb51de1ea86f12131473349e462491552
martinbednar <martin.bedn20@gmail.com>
2020-12-16 09:00:24 +0100

    Implement blocking HTTP request in Chrome like in Firefox.
    Blocking in based on DNS (private and public IP addresses) and not on statistics.

--- chrome/http_shield_chrome.js
+++ chrome/http_shield_chrome.js
@@ -30,8 +30,138 @@ var dnsCache = new Object();
 
 /// webRequest event listener, hooked to onBeforeSendHeaders event
 /// Catches all requests, analyzes them, does blocking
+/// Not possible to merge implementation of this function with implementation for Firefox,
+/// because Chrome does not support async listerners for blocking requests
+/// This function must not be asynchronous for Chrome.
+/// See more: https://stackoverflow.com/questions/47910732/browserextension-webrequest-onbeforerequest-return-promise
 function beforeSendHeadersListener(requestDetail) {
-	return {cancel: false};
+
+	//If either of information is undefined, permit it
+	//initiator happens to be undefined for the first request of the page loading the document itself
+	if (requestDetail.initiator === undefined || requestDetail.url === undefined)
+	{
+		return {cancel:false};
+	}
+	var sourceUrl = new URL(requestDetail.initiator);
+	//Removing www. from hostname, so the hostnames are uniform
+	sourceUrl.hostname = sourceUrl.hostname.replace(/^www\./,'');
+	var targetUrl = new URL(requestDetail.url);
+	//Removing www. from hostname, so the hostnames are uniform
+	targetUrl.hostname = targetUrl.hostname.replace(/^www\./,'');
+
+	var targetIP;
+	var sourceIP;
+	var isSourcePrivate = false;
+	var isDestinationPrivate = false;
+
+	//Host found among user's trusted hosts, allow it right away
+	if (checkWhitelist(sourceUrl.hostname))
+	{
+		return {cancel:false};
+	}
+
+	//Checking type of SOURCE URL
+	if (isIPV4(sourceUrl.hostname)) //SOURCE is IPV4 adddr
+	{
+		//Checking privacy of IPv4
+		if (isIPV4Private(sourceUrl.hostname))
+		{
+			//Source is IPv4 private
+			isSourcePrivate = true;
+		}
+	}
+	else if(isIPV6(sourceUrl.hostname)) //SOURCE is IPV6
+	{
+		//Checking privacy of IPv6
+		if (isIPV6Private(sourceUrl.hostname))
+		{
+			//Source is IPv6 private
+			isSourcePrivate = true;
+		}
+	}
+	else //SOURCE is hostname
+	{
+		//Search the DNS cache for the source domain
+		if (dnsCache[sourceUrl.hostname] !== undefined) {
+			//More IPs could have been found, for each of them
+			for (let ip of dnsCache[sourceUrl.hostname])
+			{
+				//Check whether it's IPv4
+				if (isIPV4(ip))
+				{
+					 if (isIPV4Private(ip))
+					 {
+						//IPv4 is private
+						isSourcePrivate = true;
+					 }
+				}
+				else if (isIPV6(ip))
+				{
+					if (isIPV6Private(ip))
+					{
+						//IPv6 is private
+						isSourcePrivate = true;
+					}
+				}
+			}
+		}
+	}
+
+	//Analyzing targetUrl
+	//Check IPv4/IPv6 and privacy
+	if (isIPV4(targetUrl.hostname))
+	{
+		if (isIPV4Private(targetUrl.hostname))
+		{
+			isDestinationPrivate = true;
+
+		}
+	}
+	else if(isIPV6(targetUrl.hostname))
+	{
+		if (isIPV6Private(targetUrl.hostname))
+		{
+			isDestinationPrivate = true;
+		}
+	}
+	else //Target is hostname
+	{
+		//Search the DNS cache for the target domain
+		if (dnsCache[targetUrl.hostname] !== undefined) {
+			//More IPs could have been found, for each of them
+			for (let ip of dnsCache[targetUrl.hostname])
+			{
+				//Check whether it's IPv4
+				if (isIPV4(ip))
+				{
+					 if (isIPV4Private(ip))
+					 {
+						//IPv4 is private
+						isDestinationPrivate = true;
+					 }
+				}
+				else if (isIPV6(ip))
+				{
+					if (isIPV6Private(ip))
+					{
+						//IPv6 is private
+						isDestinationPrivate = true;
+					}
+				}
+			}
+		}
+	}
+	
+	//Blocking direction Public -> Private
+	if (!isSourcePrivate && isDestinationPrivate)
+	{
+		notifyBlockedRequest(sourceUrl.hostname, targetUrl.hostname, requestDetail.type);
+		return {cancel:true}
+	}
+	else //Permitting others
+	{
+		return {cancel: false};
+	}
 }
 
 
