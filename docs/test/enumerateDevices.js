5d496584f8f6863a108ac6353c18d90636362058
Libor Polčák <ipolcak@fit.vutbr.cz>
2021-01-14 15:37:19 +0100

    Add test of wrapper for MediaDevices.prototype.enumerateDevices

---
+++ docs/test/enumerateDevices.js
@@ -0,0 +1,24 @@
+let ul = document.getElementById("enumerateDevices");
+
+function appendLi(text)
+{
+	let li = document.createElement("li");
+	li.innerText = text;
+	ul.appendChild(li)
+}
+
+if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+	appendLi("Your browser does not support enumerateDevices() so it does not leak IDs of cameras and microphones.");
+}
+else {
+	navigator.mediaDevices.enumerateDevices()
+	.then(function(devices) {
+		appendLi(devices.length.toString() + " cameras and microphones detected");
+		devices.forEach(function(device) {
+			appendLi(device.kind + ": " + device.label + " id = " + device.deviceId);
+		});
+	})
+	.catch(function(err) {
+		appendLi(err.name + ": " + err.message);
+	});
+}
