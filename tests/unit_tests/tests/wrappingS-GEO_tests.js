ea60d5257c1fed79e19e1fdb16dfcd89c9f6595d
martinbednar <martin.bedn20@gmail.com>
2021-01-18 17:08:13 +0100

    Add GEO unit tests for checking if GEO returns changed coords.

--- tests/unit_tests/tests/wrappingS-GEO_tests.js
+++ tests/unit_tests/tests/wrappingS-GEO_tests.js
@@ -67,18 +67,6 @@ describe("GEO", function() {
 				},
 				timestamp: 1610704131841
 			},
-			london: {
-				coords: {
-					accuracy: 150,
-					altitude: null,
-					altitudeAccuracy: null,
-					heading: null,
-					latitude: 51.507351,
-					longitude: -0.127758,
-					speed: null
-				},
-				timestamp: 1610954058112
-			},
 			san_francisco: {
 				coords: {
 					accuracy: 150,
@@ -102,6 +90,18 @@ describe("GEO", function() {
 					speed: null
 				},
 				timestamp: 1610954214124
+			},
+			london: {
+				coords: {
+					accuracy: 150,
+					altitude: null,
+					altitudeAccuracy: null,
+					heading: null,
+					latitude: 51.507351,
+					longitude: -0.127758,
+					speed: null
+				},
+				timestamp: 1610954058112
 			},
 			equator_0meridian: {
 				coords: {
@@ -212,6 +212,31 @@ describe("GEO", function() {
 				timestamp: 1610954419061
 			}
 		}
+		
+		//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+		//Source: https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+		function calcCrow(lat1, lon1, lat2, lon2) 
+		{
+		  var R = 6371; // km
+		  var dLat = toRad(lat2-lat1);
+		  var dLon = toRad(lon2-lon1);
+		  var lat1 = toRad(lat1);
+		  var lat2 = toRad(lat2);
+
+		  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+			Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+		  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+		  var d = R * c;
+		  return d;
+		}
+
+		// Converts numeric degrees to radians
+		//Source: https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+		function toRad(Value) 
+		{
+			return Value * Math.PI / 180;
+		}
+		
 		it("should be defined.",function() {
 			expect(processOriginalGPSDataObject).toBeDefined();
 		});
@@ -222,45 +247,109 @@ describe("GEO", function() {
 			}
 		});
 		it("should return given coordinates when flag provideAccurateGeolocationData is set.",function() {
-			if (typeof processOriginalGPSDataObject !== undefined) {
+			if (typeof processOriginalGPSDataObject !== undefined && typeof processOriginalGPSDataObject_globals !== undefined) {
 				eval(processOriginalGPSDataObject);
 				eval(processOriginalGPSDataObject_globals);
 				provideAccurateGeolocationData = true;
 				for (const position_key in originalPositions) {
-					expect(processOriginalGPSDataObject(undefined, originalPositions[position_key])['coords']).toBe(originalPositions[position_key]['coords']);
-					expect(processOriginalGPSDataObject(undefined, originalPositions[position_key])['timestamp']).not.toBe(originalPositions[position_key]['timestamp']);
+					expect(processOriginalGPSDataObject(undefined, originalPositions[position_key])['coords']).toEqual(originalPositions[position_key]['coords']);
+					expect(processOriginalGPSDataObject(undefined, originalPositions[position_key])['timestamp']).not.toEqual(originalPositions[position_key]['timestamp']);
 				}
 			}
 		});
 		it("should return changed timestamp when flag provideAccurateGeolocationData is set.",function() {
-			if (typeof processOriginalGPSDataObject !== undefined) {
+			if (typeof processOriginalGPSDataObject !== undefined && typeof processOriginalGPSDataObject_globals !== undefined) {
 				eval(processOriginalGPSDataObject);
 				eval(processOriginalGPSDataObject_globals);
 				provideAccurateGeolocationData = true;
 				for (const position_key in originalPositions) {
-					expect(processOriginalGPSDataObject(undefined, originalPositions[position_key])['timestamp']).not.toBe(originalPositions[position_key]['timestamp']);
+					expect(processOriginalGPSDataObject(undefined, originalPositions[position_key])['timestamp']).not.toEqual(originalPositions[position_key]['timestamp']);
 				}
 			}
 		});
 		it("should return given coordinates when previouslyReturnedCoords are set.",function() {
-			if (typeof processOriginalGPSDataObject !== undefined) {
+			if (typeof processOriginalGPSDataObject !== undefined && typeof processOriginalGPSDataObject_globals !== undefined) {
 				eval(processOriginalGPSDataObject);
 				eval(processOriginalGPSDataObject_globals);
 				provideAccurateGeolocationData = false;
 				for (const position_key in originalPositions) {
 					previouslyReturnedCoords = originalPositions[position_key]['coords'];
-					expect(processOriginalGPSDataObject(undefined, originalPositions[position_key])['coords']).toBe(originalPositions[position_key]['coords']);
+					expect(processOriginalGPSDataObject(undefined, originalPositions[position_key])['coords']).toEqual(originalPositions[position_key]['coords']);
 				}
 			}
 		});
 		it("should return changed timestamp when previouslyReturnedCoords are set.",function() {
-			if (typeof processOriginalGPSDataObject !== undefined) {
+			if (typeof processOriginalGPSDataObject !== undefined && typeof processOriginalGPSDataObject_globals !== undefined) {
 				eval(processOriginalGPSDataObject);
 				eval(processOriginalGPSDataObject_globals);
 				provideAccurateGeolocationData = false;
 				for (const position_key in originalPositions) {
 					previouslyReturnedCoords = originalPositions[position_key]['coords'];
-					expect(processOriginalGPSDataObject(undefined, originalPositions[position_key])['timestamp']).not.toBe(originalPositions[position_key]['timestamp']);
+					expect(processOriginalGPSDataObject(undefined, originalPositions[position_key])['timestamp']).not.toEqual(originalPositions[position_key]['timestamp']);
+				}
+			}
+		});
+		it("should return coords that are not equal to original coords.",function() {
+			if (typeof processOriginalGPSDataObject !== undefined && typeof processOriginalGPSDataObject_globals !== undefined) {
+				eval(processOriginalGPSDataObject);
+				eval(processOriginalGPSDataObject_globals);
+				provideAccurateGeolocationData = false;
+				previouslyReturnedCoords = undefined;
+				var desiredAccuracyArray = [ 0.1, 1, 10, 100 ]
+				var desiredAccuracy;
+				provideAccurateGeolocationData = false;
+				previouslyReturnedCoords = undefined;
+				var changed_coords;
+				var original_coords;
+				for (let i = 0; i < desiredAccuracyArray.length; i++) {
+					desiredAccuracy = desiredAccuracyArray[i];
+					for (const position_key in originalPositions) {
+						changed_coords = processOriginalGPSDataObject(undefined, originalPositions[position_key])['coords'];
+						original_coords = originalPositions[position_key]['coords'];
+						expect(changed_coords).not.toEqual(original_coords);
+						previouslyReturnedCoords = undefined;
+					}
+				}
+			}
+		});
+		it("should return changed coords that are in required accuracy.",function() {
+			if (typeof processOriginalGPSDataObject !== undefined && typeof processOriginalGPSDataObject_globals !== undefined) {
+				eval(processOriginalGPSDataObject);
+				eval(processOriginalGPSDataObject_globals);
+				provideAccurateGeolocationData = false;
+				previouslyReturnedCoords = undefined;
+				var desiredAccuracyArray = [ 0.1, 1, 10, 100 ]
+				var desiredAccuracy;
+				provideAccurateGeolocationData = false;
+				previouslyReturnedCoords = undefined;
+				var changed_coords;
+				var original_coords;
+				for (let i = 0; i < desiredAccuracyArray.length; i++) {
+					desiredAccuracy = desiredAccuracyArray[i];
+					for (const position_key in originalPositions) {
+						changed_coords = processOriginalGPSDataObject(undefined, originalPositions[position_key])['coords'];
+						original_coords = originalPositions[position_key]['coords'];
+						expect(calcCrow(changed_coords['latitude'], changed_coords['longitude'], original_coords['latitude'], original_coords['longitude'])).toBeLessThan(10*desiredAccuracy);
+						previouslyReturnedCoords = undefined;
+					}
+				}
+			}
+		});
+		it("should return changed timestamp.",function() {
+			if (typeof processOriginalGPSDataObject !== undefined && typeof processOriginalGPSDataObject_globals !== undefined) {
+				eval(processOriginalGPSDataObject);
+				eval(processOriginalGPSDataObject_globals);
+				provideAccurateGeolocationData = false;
+				previouslyReturnedCoords = undefined;
+				var desiredAccuracyArray = [ 0.1, 1, 10, 100 ]
+				var desiredAccuracy;
+				for (let i = 0; i < desiredAccuracyArray.length; i++) {
+					desiredAccuracy = desiredAccuracyArray[i];
+					for (const position_key in originalPositions) {
+						let changed_timestamp = processOriginalGPSDataObject(undefined, originalPositions[position_key])['timestamp'];
+						let original_timestamp = originalPositions[position_key]['timestamp'];
+						expect(changed_timestamp).not.toEqual(original_timestamp);
+					}
 				}
 			}
 		});
