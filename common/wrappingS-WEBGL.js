8632af4ee334d36171a6f0f4b90c2d4e6b146cdb
xsvanc06 <xsvanc06@stud.fit.vutbr.cz>
2021-03-06 13:08:27 +0100

    wWEBGL: doxy comments

--- common/wrappingS-WEBGL.js
+++ common/wrappingS-WEBGL.js
@@ -24,19 +24,53 @@
 //  This Source Code Form is subject to the terms of the Mozilla Public
 //  License, v. 2.0. If a copy of the MPL was not distributed with this file,
 //  You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//  Copyright (c) 2020 The Brave Authors.
 
-/**
-*
-* This algorithm is a modified version of the similar algorithm from
-* Brave Software <https://brave.com>
-* available at <https://github.com/brave/brave-core/blob/master/chromium_src/third_party/blink/renderer/modules/webgl/webgl2_rendering_context_base.cc>
-* Copyright (c) 2020 The Brave Authors.
-*/
+/** \file
+ * This file contains wrappers for WebGL related calls
+ *  * https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext
+ *  * https://developer.mozilla.org/en-US/docs/Web/API/WebGL2RenderingContext
+ * \ingroup wrappers
+ *
+ * The goal is to prevent fingerprinting by modifying the values from certain WebGLRenderingContext API functions.
+ * This includes return values of various functions which can be hardware/software specific and image data reading.
+ *
+ * Content is either modified according to domain and session keys to be different than the original albeit very similar
+ * or replaced by bottom value which is consistent every time.
+ * Both approaches are inspired by the algorithms created by Brave Software <https://brave.com>
+ * available at [https://github.com/brave/brave-core/blob/master/chromium_src/third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc,
+ * https://github.com/brave/brave-core/blob/master/chromium_src/third_party/blink/renderer/modules/webgl/webgl2_rendering_context_base.cc]
+ *
+ * This wrapper operates with two levels of protection:
+ *	* (0) - return modified results, such as slightly changed image, slightly changed number or random string
+ *	* (1) - return bottom values - such as zero, empty string, empty image, null, etc.
+ *
+ * Level 0 is trying to force WebGL fingeprint to be unique on every domain and every session. This can be effective
+ * when used with other wrappers with same options. This level causes breakage of websites using WebGL only rarely.
+ * Level 1 is trying to return as little information as possible while being consistent across domains and sessions.
+ * This level can cause breakage on majority of websites using WebGL.
+ *
+ * Also note that both approaches are detectable by a fingerprinter that checks if a predetermined image
+ * is the same as the read one or if specific function returns expected value.
+ * Nevertheless, the aim of the wrappers is to limit the finerprintability.
+ *
+ */
 /*
  * Create private namespace
  */
 (function() {
-
+	/** @var String farbleWebGLparam
+	 * \brief Modifies WebGLRenderingContext.getParameter return value for some specific parameters, original value for the rest
+	 *
+	 * \param pname GLEnum (https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Constants)
+	 * \param ctx WebGLRenderingContext (https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext)
+	 *
+	 * Also contains helping functions farbleGLint and randomString which are used in farbleWebGLparam
+	 * Depending on level chosen this function returns:
+	 *	* (0) - depending on type - either changed original number or random string
+	 *	* (1) - bottom value depending on parameter [null, 0, empty string, random string]
+	 */
 	var farbleWebGLparam = `
 		function farbleGLint(number){
 			var ret = 0;
@@ -136,6 +170,17 @@
 			}
 		}
 	`;
+	/** @var String farbleNull
+	 * \brief Modifies return value
+	 *
+	 * \param name String name of original function
+	 * \param ctx WebGLRenderingContext (https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext)
+	 * \param ...fcarg delegated arguments depending on function
+	 *
+	 * Depending on level chosen this function returns:
+	 *	* (0) - original value
+	 *	* (1) - null
+	 */
 	var farbleNull = `
 		function farbleNull(name, ctx, ...fcarg){
 			if(args[0]===1){
@@ -145,7 +190,17 @@
 				return eval(name+".call(ctx, ...fcarg);");
 			}
 		}`;
-
+	/** @var String farbleZero
+	 * \brief Modifies return value
+	 *
+	 * \param name String name of original function
+	 * \param ctx WebGLRenderingContext (https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext)
+	 * \param ...fcarg delegated arguments depending on function
+	 *
+	 * Depending on level chosen this function returns:
+	 *	* (0) - original value
+	 *	* (1) - zero
+	 */
 	var farbleZero = `
 		function farbleZero(name, ctx, ...fcarg){
 			if(args[0]===1){
@@ -155,7 +210,17 @@
 				return eval(name+".call(ctx, ...fcarg);");
 			}
 		}`;
-
+	/** @var String farbleMinusOne
+	 * \brief Modifies return value
+	 *
+	 * \param name String name of original function
+	 * \param ctx WebGLRenderingContext (https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext)
+	 * \param ...fcarg delegated arguments depending on function
+	 *
+	 * Depending on level chosen this function returns:
+	 *	* (0) - original value
+	 *	* (1) - minus one
+	 */
 	var farbleMinusOne = `
 		function farbleMinusOne(name, ctx, ...fcarg){
 			if(args[0]===1){
@@ -165,6 +230,17 @@
 				return eval(name+".call(ctx, ...fcarg);");
 			}
 		}`;
+	/** @var String farbleNullArray
+	 * \brief Modifies return value
+	 *
+	 * \param name String name of original function
+	 * \param ctx WebGLRenderingContext (https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext)
+	 * \param ...fcarg delegated arguments depending on function
+	 *
+	 * Depending on level chosen this function returns:
+	 *	* (0) - original value
+	 *	* (1) - empty array
+	 */
 	var farbleNullArray = `
 		function farbleNullArray(name, ctx, ...fcarg){
 			if(args[0]===1){
@@ -195,6 +271,17 @@
 				return origGetShaderPrecisionFormat.call(ctx, ...fcarg);
 			}
 		}`;
+	/** @var String farbleGetActives
+	 * \brief Modifies return value
+	 *
+	 * \param name String name of original function
+	 * \param ctx WebGLRenderingContext (https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext)
+	 * \param ...fcarg delegated arguments depending on function
+	 *
+	 * Depending on level chosen this function returns:
+	 *	* (0) - original value
+	 *	* (1) - WebGLActiveInfo object with empty attributes
+	 */
 	var farbleGetActives = `
 		function farbleGetActives(name, ctx, ...fcarg){
 			if(args[0]===1){
@@ -372,6 +459,24 @@
 			}
 		}
 	`;
+	/** @var String farblePixels
+	 * \brief Modifies pixels array
+	 *
+	 * \param ctx WebGLRenderingContext (https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext)
+	 * \param x starting x position
+	 * \param y starting y position
+	 * \param width width to be read (in pixels)
+	 * \param height height to be read (in pixels)
+	 * \param format format of read pixel data
+	 * \param type specifies data type of pixels
+	 * \param pixels typed array to return result
+	 * \param offset optional offset
+	 *
+	 * Also contains helping function lfsr_next which is used to determine which pixels are changed
+	 * Depending on level chosen this function returns to pixels array:
+	 *	* (0) - slightly changed image data according to domain and session keys
+	 *	* (1) - empty array
+	 */
 	var farblePixels = `
 		function lfsr_next(v) {
 			return BigInt.asUintN(64, ((v >> 1n) | (((v << 62n) ^ (v << 61n)) & (~(~0n << 63n) << 62n))));
@@ -412,6 +517,13 @@
 			helping_code: farbleWebGLparam,
 			original_function: "parent.WebGLRenderingContext.prototype.getParameter",
 			wrapping_function_args: "constant",
+			/** \fn fake WebGLRenderingContext.prototype.getParameter
+			 * \brief Modifies return value for some specific parameters, original value for the rest
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - depending on type - either changed original number or random string
+			 *	* (1) - bottom value depending on parameter [null, 0, empty string, random string]
+			 */
 			wrapping_function_body: `
 				return farbleWebGLparam(this,constant);
 			`,
@@ -428,6 +540,13 @@
 			helping_code: farbleWebGLparam,
 			original_function: "parent.WebGL2RenderingContext.prototype.getParameter",
 			wrapping_function_args: "constant",
+			/** \fn fake WebGL2RenderingContext.prototype.getParameter
+			 * \brief Modifies return value for some specific parameters, original value for the rest
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - depending on type - either changed original number or random string
+			 *	* (1) - bottom value depending on parameter [null, 0, empty string, random string]
+			 */
 			wrapping_function_body: `
 				return farbleWebGLparam(this,constant);
 			`,
@@ -444,6 +563,13 @@
 			helping_code: farbleNullArray,
 			original_function: "parent.WebGLRenderingContext.prototype.getSupportedExtensions",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getSupportedExtensions
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - empty array
+			 */
 			wrapping_function_body: `
 				return farbleNullArray("origGetSupportedExtensions", this, ...args);
 			`,
@@ -460,6 +586,13 @@
 			helping_code: farbleGetActives,
 			original_function: "parent.WebGLRenderingContext.prototype.getActiveAttrib",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getActiveAttrib
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - WebGLActiveInfo object with empty attributes
+			 */
 			wrapping_function_body: `
 				return farbleGetActives("origGetActiveAttrib", this, ...args);
 			`,
@@ -476,6 +609,13 @@
 			helping_code: farbleNull,
 			original_function: "parent.WebGLRenderingContext.prototype.getExtension",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getExtension
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - null
+			 */
 			wrapping_function_body: `
 				return farbleNull("origGetExtension", this, ...args);
 			`,
@@ -492,6 +632,13 @@
 			helping_code: farbleGetActives,
 			original_function: "parent.WebGLRenderingContext.prototype.getActiveUniform",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getActiveUniform
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - WebGLActiveInfo object with empty attributes
+			 */
 			wrapping_function_body: `
 				return farbleGetActives("origGetActiveUniform", this, ...args);
 			`,
@@ -508,6 +655,13 @@
 			helping_code: farbleNull,
 			original_function: "parent.WebGLRenderingContext.prototype.getUniformLocation",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getUniformLocation
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - null
+			 */
 			wrapping_function_body: `
 				return farbleNull("oriGetUniformLocation", this, ...args);
 			`,
@@ -524,6 +678,13 @@
 			helping_code: farbleMinusOne,
 			original_function: "parent.WebGLRenderingContext.prototype.getAttribLocation",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getAttribLocation
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - minus one
+			 */
 			wrapping_function_body: `
 				return farbleMinusOne("origGetAttribLocation", this, ...args);
 			`,
@@ -540,6 +701,13 @@
 			helping_code: farbleZero,
 			original_function: "parent.WebGLRenderingContext.prototype.getVertexAttribOffset",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getVertexAttribOffset
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - zero
+			 */
 			wrapping_function_body: `
 				return farbleZero("origGetVertexAttribOffset", this, ...args);
 			`,
@@ -556,6 +724,13 @@
 			helping_code: farblePixels,
 			original_function: "parent.WebGLRenderingContext.prototype.readPixels",
 			wrapping_function_args: "x, y, width, height, format, type, pixels, offset",
+			/** \fn fake WebGLRenderingContext.prototype.readPixels
+			 * \brief Modifies array which contains result of readPixels
+			 *
+			 * Depending on level chosen modifies result array:
+			 *	* (0) - change values according to domain and session hashes
+			 *	* (1) - leave array empty
+			 */
 			wrapping_function_body: `
 				farblePixels(this, x, y, width, height, format, type, pixels, offset);
 			`,
@@ -572,6 +747,13 @@
 			helping_code: farblePixels,
 			original_function: "parent.WebGL2RenderingContext.prototype.readPixels",
 			wrapping_function_args: "x, y, width, height, format, type, pixels, offset",
+			/** \fn fake WebGL2RenderingContext.prototype.readPixels
+			 * \brief Modifies array which contains result of readPixels
+			 *
+			 * Depending on level chosen modifies result array:
+			 *	* (0) - change values according to domain and session hashes
+			 *	* (1) - leave array empty
+			 */
 			wrapping_function_body: `
 				farblePixels(this, x, y, width, height, format, type, pixels, offset);
 			`,
@@ -588,6 +770,13 @@
 			helping_code: farbleGetFramebufferAttachmentParameter,
 			original_function: "parent.WebGLRenderingContext.prototype.getFramebufferAttachmentParameter",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getFramebufferAttachmentParameter
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - bottom value depending on parameter [34069, 9729, 5124, 0]
+			 */
 			wrapping_function_body: `
 				return farbleGetFramebufferAttachmentParameter(this, ...args);
 			`,
@@ -604,6 +793,13 @@
 			helping_code: farbleGetBufferParameter,
 			original_function: "parent.WebGLRenderingContext.prototype.getBufferParameter",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getBufferParameter
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - bottom value depending on parameter [35044, 0]
+			 */
 			wrapping_function_body: `
 				return farbleGetBufferParameter(this, ...args);
 			`,
@@ -620,6 +816,13 @@
 			helping_code: farbleGetProgramParameter,
 			original_function: "parent.WebGLRenderingContext.prototype.getProgramParameter",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getProgramParameter
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - bottom value depending on parameter [false, 35981, 0]
+			 */
 			wrapping_function_body: `
 				return farbleGetProgramParameter(this, ...args);
 			`,
@@ -636,6 +839,13 @@
 			helping_code: farbleGetRenderbufferParameter,
 			original_function: "parent.WebGLRenderingContext.prototype.getRenderbufferParameter",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getRenderbufferParameter
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - bottom value depending on parameter [32854, 0]
+			 */
 			wrapping_function_body: `
 				return farbleGetRenderbufferParameter(this, ...args);
 			`,
@@ -652,6 +862,13 @@
 			helping_code: farbleGetShaderParameter,
 			original_function: "parent.WebGLRenderingContext.prototype.getShaderParameter",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getShaderParameter
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - bottom value depending on parameter [35633, false]
+			 */
 			wrapping_function_body: `
 				return farbleGetShaderParameter(this, ...args);
 			`,
@@ -668,6 +885,13 @@
 			helping_code: farbleGetVertexAttrib,
 			original_function: "parent.WebGLRenderingContext.prototype.getVertexAttrib",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getVertexAttrib
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - bottom value depending on parameter [null, 0, false, 5120, empty array]
+			 */
 			wrapping_function_body: `
 				return farbleGetVertexAttrib(this, ...args);
 			`,
@@ -684,6 +908,13 @@
 			helping_code: farbleZero,
 			original_function: "parent.WebGLRenderingContext.prototype.getUniform",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getUniform
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - zero
+			 */
 			wrapping_function_body: `
 				return farbleZero("origGetUniform", this, ...args);
 			`,
@@ -700,6 +931,13 @@
 			helping_code: farbleNull,
 			original_function: "parent.WebGLRenderingContext.prototype.getTexParameter",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getTexParameter
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - null - as if error has occured
+			 */
 			wrapping_function_body: `
 				return farbleNull("origGetTexParameter", this, ...args);
 			`,
@@ -716,6 +954,13 @@
 			helping_code: farbleGetPrecisionFormat,
 			original_function: "parent.WebGLRenderingContext.prototype.getShaderPrecisionFormat",
 			wrapping_function_args: "...args",
+			/** \fn fake WebGLRenderingContext.prototype.getShaderPrecisionFormat
+			 * \brief Modifies return value
+			 *
+			 * Depending on level chosen this function returns:
+			 *	* (0) - original return value
+			 *	* (1) - WebGLShaderPrecisionFormat object with empty attributes
+			 */
 			wrapping_function_body: `
 				return farbleGetPrecisionFormat(this, ...args);
 			`,
