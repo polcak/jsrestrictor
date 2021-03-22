c207f52afe50a520d37ce4d2d644d9cab7aea886
xsvanc06 <xsvanc06@stud.fit.vutbr.cz>
2021-03-22 19:02:47 +0100

    wDM: doxy comments

--- common/wrappingS-DM.js
+++ common/wrappingS-DM.js
@@ -19,6 +19,30 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
+//  Alternatively, the contents of this file may be used under the terms
+//  of the Mozilla Public License, v. 2.0, as described below:
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this file,
+//  You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//  Copyright (c) 2020 The Brave Authors.
+
+/** \file
+ * This file contains wrapper for navigator.deviceMemory https://developer.mozilla.org/en-US/docs/Web/API/Navigator/deviceMemory
+ * \ingroup wrappers
+ *
+ * The goal is to prevent fingerprinting by modifying return value of navigator.deviceMemory parameter.
+ *
+ * This wrapper operates with three levels of protection:
+ *	* (0) - return random valid value from range [0.25 - real value]
+ *	* (1) - return random valid value from range [0.25 - 8]
+ *	* (2) - return 4
+ *
+ * These approaches are inspired by the algorithms created by Brave Software <https://brave.com>
+ * available at https://github.com/brave/brave-core/blob/master/chromium_src/third_party/blink/renderer/core/frame/navigator_device_memory.cc
+ *
+ */
 
 /*
  * Create private namespace
@@ -53,6 +77,13 @@
 					parent_object: "navigator",
 					parent_object_property: "deviceMemory",
 					wrapped_objects: [],
+					/**  \brief replaces navigator.deviceMemory getter
+					 *
+					 * Depending on level chosen this property returns:
+					 *	* (0) - random valid value from range [0.25 - real value]
+					 *	* (1) - random valid value from range [0.25 - 8]
+					 *	* (2) - 4
+					 */
 					wrapped_properties: [
 						{
 							property_name: "get",
