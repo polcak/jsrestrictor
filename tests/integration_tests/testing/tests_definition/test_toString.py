f5445c4cd3b2a5bd74a0a732740186a314545e7d
martinbednar <martin.bedn20@gmail.com>
2021-03-26 14:21:11 +0100

    Integration tests: Add test for methods.toString (comparing methods.toString with and without JSR). JSR should not change methods.toString.

---
+++ tests/integration_tests/testing/tests_definition/test_toString.py
@@ -0,0 +1,39 @@
+#
+#  JavaScript Restrictor is a browser extension which increases level
+#  of security, anonymity and privacy of the user while browsing the
+#  internet.
+#
+#  Copyright (C) 2021  Martin Bednar
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import pytest
+
+from values_getters import get_methods_toString
+
+
+## Setup method - it is run before toString tests execution starts.
+#
+#  This setup method initialize variable methods_toString that contains methods.toString() and
+#  this variable is provided to methods_toString test and the methods.toString() in the variable are compared with real values.
+@pytest.fixture(scope='module', autouse=True)
+def methods_toString(browser):
+	return get_methods_toString(browser.driver)
+
+
+## Test methods.toString(). They should be always unchanged by JSR.
+def test_methods_toString(browser, methods_toString):
+	for method in methods_toString:
+		assert methods_toString[method] == browser.real.methods_toString[method]
