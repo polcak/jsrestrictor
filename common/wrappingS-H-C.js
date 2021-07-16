85bcdbdd558e89653ca4ab298751623b7b753ce4
Ricardo Lafuente <r@manufacturaindependente.org>
2021-07-16 09:21:22 +0100

    Merge for rebasing

--- common/wrappingS-H-C.js
+++ common/wrappingS-H-C.js
@@ -35,19 +35,21 @@
 //  \copyright Copyright (c) 2020 The Brave Authors.
 
 /** \file
+ * This file contains wrappers for calls related to the Canvas API, about which you can read more at MDN:
+ *  * [Canvas API](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API)
+ *  * [CanvasRenderingContext2D](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D)
+ *  * [OffscreenCanvas](https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas)
  *
  * The goal is to prevent fingerprinting by modifying the values that can be read from the canvas.
  * So the visual content of wrapped canvases as displayed on the screen is the same as intended.
  *
  * The modified content can be either an empty image or a fake image that is modified according to
  * session and domain keys to be different than the original albeit very similar (i.e. the approach
- * inspired by the algorithms created by Brave Software <https://brave.com>
- * available at https://github.com/brave/brave-core/blob/master/chromium_src/third_party/blink/renderer/core/execution_context/execution_context.cc.
+ * inspired by the algorithms created by [Brave Software](https://brave.com) available [here](https://github.com/brave/brave-core/blob/master/chromium_src/third_party/blink/renderer/core/execution_context/execution_context.cc).
  *
  * Note that both approaches are detectable by a fingerprinter that checks if a predetermined image
- * inserted to the canvas is the same as the read one, see for example,
- * https://arkenfox.github.io/TZP/tests/canvasnoise.html. Nevertheless, the aim of the wrappers is
- * to limit the finerprintability.
+ * inserted to the canvas is the same as the read one, see [here](https://arkenfox.github.io/TZP/tests/canvasnoise.html) for an example,
+ * Nevertheless, the aim of the wrappers is to limit the finerprintability.
  *
  * Also note that a determined fingerprinter can reveal the modifications and consequently uncover
  * the original image. This can be avoided with the approach that completely clears the data stored
