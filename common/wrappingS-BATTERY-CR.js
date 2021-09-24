ae7f62dcdcdf7d078bf3cbae5987f7e32da5ee0c
Libor Polčák <ipolcak@fit.vutbr.cz>
2021-09-24 16:19:03 +0200

    Document BATTERY wrappers file

--- common/wrappingS-BATTERY-CR.js
+++ common/wrappingS-BATTERY-CR.js
@@ -22,6 +22,24 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
+/** \file
+ * \ingroup wrappers
+ *
+ * The `navigator.getBattery()` reports the state of the battery and can be
+ * misused to fingerprint users for a short term. The API was removed from
+ * Firefox.
+ *
+ * \see https://lukaszolejnik.com/battery
+ *
+ * The API is still supported in browsers derived from Chromium. The wrapper
+ * mimics Firefox behaviour.
+ *
+ * \bug Because we mimic Firefox behaviour, a Chromium derived browser
+ * becomes more easily fingerprintable. This can be fixed by properly
+ * wrapping `BatteryManager.prototype` getters and setters.
+ */
+
+
 /*
  * Create private namespace
  */
