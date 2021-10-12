eabe50319d547a4f5c786d39ea81075af6e30044
Ricardo Lafuente <r@manufacturaindependente.org>
2021-10-12 11:08:50 +0100

    Generate RSS feeds in development

--- website/pelicanconf.py
+++ website/pelicanconf.py
@@ -39,13 +39,6 @@ TIMEZONE = "Europe/Paris"
 
 DEFAULT_LANG = "en"
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
 DEFAULT_PAGINATION = 15
 
 # Uncomment following line if you want document-relative URLs when developing
