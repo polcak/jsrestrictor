1ef01a4a1872b3c7d08ce987dabfb37926fd937e
Radek Hranicky <ihranicky@fit.vutbr.cz>
2021-11-18 16:01:53 +0100

    Added sine-based generating of Magnetometer values

--- common/wrappingS-SENSOR-MAGNET.js
+++ common/wrappingS-SENSOR-MAGNET.js
@@ -25,7 +25,8 @@
  /** \file
   * \ingroup wrappers
   *
-
+  * TODO: document
+  * TODO: add movement simulation of non-stationary devices
   */
 
   /*
@@ -33,115 +34,166 @@
    */
 (function() {
 
-/*
-  const FIELD_MIN = 30;
-  const FIELD_MAX = 70;
-  const FLUCTUATION = 0.05; // [0,1)
-
-  class Reading {
-    constructor() {
-      this.orig_x = null;
-      this.orig_y = null;
-      this.orig_z = null;
-      this.timestamp = null; // To distinguish between readings
-
-      this.fake_x = null;
-      this.fake_y = null;
-      this.fake_z = null;
-    }
-  }
-*/
-
-/*
-  function getRandomDeviceOrientation() {
-  }
-*/
-
-  // TODO: replace "var" with "get" after testing.
-
   var init_data = `
     var currentReading = currentReading || {orig_x: null, orig_y: null, orig_z: null, timestamp: null,
                       fake_x: null, fake_y: null, fake_z: null};
-
     var previousReading = previousReading || {orig_x: null, orig_y: null, orig_z: null, timestamp: null,
                       fake_x: null, fake_y: null, fake_z: null};
-
-    //let emulateStationaryDevice = args[0];
-    //let deviceOrientation = null;
-
+    var emulateStationaryDevice = (typeof args === 'undefined') ? true : args[0];
     var debugMode = false;
     `;
 
-   var orig_getters = `
+  var orig_getters = `
     var origGetX = Object.getOwnPropertyDescriptor(Magnetometer.prototype, "x").get;
     var origGetY = Object.getOwnPropertyDescriptor(Magnetometer.prototype, "y").get;
     var origGetZ = Object.getOwnPropertyDescriptor(Magnetometer.prototype, "z").get;
     var origGetTimestamp = Object.getOwnPropertyDescriptor(Sensor.prototype, "timestamp").get;
     `;
 
-  function generateRandomField(previous) {
-    const FIELD_MIN = 30;
-    const FIELD_MAX = 70;
-    const FLUCTUATION = 0.05; // [0,1)
-
-    let maxStep = previous * FLUCTUATION;
-    let min = previous - maxStep;
-    let max = previous + maxStep;
-    min = Math.max(min, FIELD_MIN);
-    max = Math.min(max, FIELD_MAX);
-
-    return Math.random() * (max - min) + min;
-    // TODO ADD SOME CENTER VALUE
+  // Generates a 32-bit from a string. Inspired by MurmurHash3 algorithm
+  // See: https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp
+  function generateSeed(s) {
+    var h;
+    for(var i = 0, h = 1779033703 ^ s.length; i < s.length; i++)
+      h = Math.imul(h ^ s.charCodeAt(i), 3432918353),
+      h = h << 13 | h >>> 19;
+    return h;
   }
 
+  // PRNG based on Mulberry32 algorithm
+  // See: https://gist.github.com/tommyettinger/46a874533244883189143505d203312c
+  function prng() {
+    // expects "seed" variable to be a 32-bit value
+    var t = seed += 0x6D2B79F5;
+    t = Math.imul(t ^ t >>> 15, t | 1);
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+  }
 
-  function fakeReading(current, previous) {
-    /*
-    current.orig_x = 30;
-    current.orig_y = 40;
-    current.orig_z = 5;
-    current.timestamp = 1000;
+  function initFieldGenerator() {
+    // Specifies, how much the values may (pseudorandomly) oscillate,
+    // i.e., how much the may relatively differ from the chosen center value
+    // in both positiva and negative way
+    const FLUCTUATION_MIN = 0.05;
+    const FLUCTUATION_MAX = 0.10;
+    const AXES_OSCILLATE_DIFFERENTLY = true;
 
-    previous.orig_x = 29;
-    previous.orig_y = 41;
-    previous.orig_z = 5;
-    previous.timestamp = 1000;
-    */
+    // Shifts the phase of each axis randomly [0, 2*PI)
+    const RANDOM_PHASE_SHIFT = true;
 
-    //console.log(previous);
+    // TODO: seed from session hash
+    let m = generateBaseField();
+    baseX = generateAxisBase();
+    baseY = generateAxisBase();
+    baseZ = generateAxisBase();
 
-    // Get strength of the previously-measured magnetic field in microTesla
-    previousM = Math.sqrt(Math.pow(previous.orig_x,2) + Math.pow(previous.orig_y,2) + Math.pow(previous.orig_z,2));
+    baseX2 = Math.pow(baseX,2)
+    baseY2 = Math.pow(baseY,2)
+    baseZ2 = Math.pow(baseZ,2)
 
-    //console.log(previousM);
+    // The total magnetic field strength is calculated as:
+    //   m = sqrt(x^2, y^2, z^2)
+    // where x,y,z are strengs in individual directions (axes).
+    //
+    // For x,y,z, the algorithm generates a sine-based fluctuation around
+    // a center value for each axis. For axis x, it is calculated as:
+    //   x = baseX * multiplier
+    //
+    // At this moment, we have calculate the basis (-1,1) for each axis.
+    // Now, we calculate the multiplier:
+    //
+    //                   m + sqrt(baseX^2 + baseY^2 + baseZ^2)
+    // multiplier = +/- -------------------------------------
+    //                       baseX^2 + baseY^2 + baseZ^2
+    //
+    // Values at axis X will oscillate around: baseX * multiplier, etc.
 
-    // Generate the new desired (fake) magnetic field
-    fakeM = generateRandomField(previousM)
+    mult = (m * Math.sqrt(baseX2 + baseY2 + baseZ2))
+                       / (baseX2 + baseY2 + baseZ2);
 
-    //console.log(fakeM);
+    fieldGen = {
+      baseField: m,
+      multiplier: mult,
+      x: {
+        base: baseX,
+        center: baseX * mult,
+        amplitude: 1,
+        shift: 0,
+        value: null
+      },
+      y: {
+        base: baseY,
+        center: baseY * mult,
+        amplitude: 1,
+        shift: 0,
+        value: null
+      },
+      z: {
+        base: baseZ,
+        center: baseZ * mult,
+        amplitude: 1,
+        shift: 0,
+        value: null
+      },
+      // Update x/y/z values based on timestamp
+      update: function(t) {
+        // Simulate the magnetic field fluctuation based on settings
+        this.x.value = Math.sin(t + this.x.shift) * this.x.amplitude + this.x.center;
+        this.y.value = Math.sin(t + this.y.shift) * this.y.amplitude + this.y.center;
+        this.z.value = Math.sin(t + this.z.shift) * this.z.amplitude + this.z.center;
+      }
+    }
 
-    fakeM2 = Math.pow(fakeM,2);
-    //console.log(fakeM2);
+    // Define amplitude based on axes oscillation
+    var minmaxFluct = FLUCTUATION_MIN - FLUCTUATION_MAX;
+    if (AXES_OSCILLATE_DIFFERENTLY) {
+      let fluctiationFactor = prng() * (minmaxFluct) + FLUCTUATION_MAX;
+      fieldGen.x.amplitude = fieldGen.x.center * fluctiationFactor;
+      fluctiationFactor = prng() * (minmaxFluct) + FLUCTUATION_MAX;
+      fieldGen.y.amplitude = fieldGen.y.center * fluctiationFactor;
+      fluctiationFactor = prng() * (minmaxFluct) + FLUCTUATION_MAX;
+      fieldGen.z.amplitude = fieldGen.z.center * fluctiationFactor;
+    } else {
+      let fluctiationFactor = prng() * (minmaxFluct) + FLUCTUATION_MAX;
+      fieldGen.x.amplitude = fieldGen.x.center * fluctiationFactor;
+      fieldGen.y.amplitude = fieldGen.y.center * fluctiationFactor;
+      fieldGen.z.amplitude = fieldGen.z.center * fluctiationFactor;
+    }
 
-    // SPLIT IT
-    // TODO:
-    portionX = fakeM2 / 3;
-    portionY = portionX;
-    portionZ = portionX;
-    // END TODO
+    // Define phase shift
+    if (RANDOM_PHASE_SHIFT) {
+      let twopi = 2 * Math.PI;
+      fieldGen.x.shift = prng() * (- twopi) + twopi;
+      fieldGen.y.shift = prng() * (- twopi) + twopi;
+      fieldGen.z.shift = prng() * (- twopi) + twopi;
+    }
 
-    current.fake_x = Math.sqrt(portionX);
-    current.fake_y = Math.sqrt(portionY);
-    current.fake_z = Math.sqrt(portionZ);
+    return fieldGen;
+  }
 
-    //console.log(current);
+  function generateBaseField() {
+    // Generate a random base field
+    const FIELD_MIN = 30;
+    const FIELD_MAX = 60;
+    return prng() * (FIELD_MIN - FIELD_MAX) + FIELD_MAX;
+  }
+
+  function generateAxisBase() {
+    // Returns a number in (-1,1)
+    var v = prng(); // Random in [0,1)
+    v *= Math.round(prng()) ? 1 : -1; // 50% change for positive / negative
+    return v;
   }
 
   function updateReadings(sensorObject) {
+    // We need the original reading's timestamp to see if it differs
+    // from the previous sample. If so, we need to update the faked x,y,z
     let currentTimestamp = origGetTimestamp.call(sensorObject);
 
     if (debugMode) {
-      console.log("[!] Debug mode: overriding timestamp")
+      // [!] Debug mode: overriding timestamp
+      // This allows test suites to set a custom timestamp externally
+      // by modifying the property of the Magnetometer object directly.
       currentTimestamp = sensorObject.timestamp;
     }
 
@@ -150,7 +202,9 @@
       return;
     }
 
-    // Update current with
+    // Update current reading
+    // NOTE: Original values are also stored for possible future use
+    //       in improvements of the magnetic field generator
     currentReading.orig_x = origGetX.call(sensorObject);
     currentReading.orig_y = origGetY.call(sensorObject);
     currentReading.orig_z = origGetZ.call(sensorObject);
@@ -159,17 +213,20 @@
     // Rotate the readings: previous <- current
     previousReading = JSON.parse(JSON.stringify(currentReading));
 
-    // Get new current reading
-    fakeReading(currentReading, previousReading);
-
-    /*
-    console.log(currentReading);
-    console.log(previousReading);
-    */
+    fieldGenerator.update(currentTimestamp);
+    currentReading.fake_x = fieldGenerator.x.value;
+    currentReading.fake_y = fieldGenerator.y.value;
+    currentReading.fake_z = fieldGenerator.z.value;
   }
 
-  var helping_functions = generateRandomField + fakeReading + updateReadings;
-  var hc = init_data + orig_getters + helping_functions;
+  var generators = `
+    var seed = seed || generateSeed(Hashes.sessionHash);
+    var fieldGenerator = fieldGenerator || initFieldGenerator();
+    `;
+
+  var helping_functions = generateSeed + prng + initFieldGenerator
+          + generateBaseField + generateAxisBase + updateReadings;
+  var hc = init_data + orig_getters + helping_functions + generators;
 
 	var wrappers = [
 		{
@@ -256,41 +313,3 @@
 	]
   	add_wrappers(wrappers);
 })()
-
-/*
-* ````````````````````````````````````````````````````````````````````````````````````````````````````
-* ````````````````````````````````````````````````````````````````````````````````````````````````````
-* ``````````````````````````````````````````````.:///+++oo/::.````````````````````````````````````````
-* `````````````````````````````````````````.:+sydmNmyo+/oydNMNdo.`````````````````````````````````````
-* ```````````````````````````````````.:ooosmNNNNMMMMdhhdydMMMMMMd:````````````````````````````````````
-* ``````````````````````````````````-dddyyNMNmmNMMMMMMMMMMMMMMMMMN:```````````````````````````````````
-* `````````````````````````````````-mMNNdmMmhddddmNMMMMMMMMMMMMMMMo```````````````````````````````````
-* ````````````````````````````````.hMMMMdss:...--:/+shdmNMMMMMMMMMNo``````````````````````````````````
-* ````````````````````````````````-NMMm+.`````````..--:/+oyNMMMMMMMm``````````````````````````````````
-* `````````````````````````````````sNd:`` ` ``````..--:::/+mMMMMMMMmo`````````````````````````````````
-* ``````````````````````````````````:.````````````...-:::/+dMMMMMMMM/`````````````````````````````````
-* `````````````````````...``````````-```   ````.-::::::////oNMMMMMMN:`````````````````````````````````
-* ``````````````.```..```..`````````:-`.--...:ohmNNmhysoo+//sNMMMMdo+o.```````````````````````````````
-* ``````````````..```````.``````````./smmNd../ohmdoyyyy+////+sMMmyy+/h/```````````````````````````````
-* ....``````````..``...``...`````````-ssoys`.://:-------://++ohdoddy+y-````````````````````````````` `
-* ....``````````.``.....`.```````````.....``.::--......-://++o++sdNd+-````````````````````````````````
-* `...```...````...``..``....`````````.`````.-:::-...--://+++++sy++/.`````````````````````````````````
-* `..`.````.````````...........````````..```-/oos:---:://++++ooho:.```````````````````````````````````
-* ``..````..````````..........``````````..`:hNMMms+/:://++++ooooo.````````````````````````````````````
-* ```````.`.``````````.```....`.````````../ymmdNNNNmy+//+++ooosoo.`````````````````````````````` `````
-* ````````.`.``````````.`.....```````````.osoo+oooo+///+ooooossos-```````````````````````````````  ```
-* `````````````````````````.``````````````--.-/oo+////+oosssyysshs/.``````````````````````````````````
-* `````````````````````````````````````````...-----::/osso++/::---..``````````````````````````````````
-* ``````````````````````````````````````````....-:/osys+::-.....-/+shy-````````````````````````````` `
-* ````````````````````````````````````````````.:/ooo+/://++oshdmNNMMMMd.``````````````````````````````
-* ```````````````````.```````````````````..`   `.--::+ydNNMMMMMMMMMMMMMh``````````````````````````````
-* `````````````````.```````````````````-/ydh+.`.-/+ymNMMMMMMMMMMMMMMMMMM/`````````````````````````````
-* ````````````````````````.`````````.:ohmNNmhooshdmNmNNNNNNNMMMMMMMMMMMMm+````````````````````````````
-* `````````````````````````````.-:/+yhddddhhdmhddmNMMNNNNNNMMMMMMMMMMMMMMMh:``````````````````````````
-* ````````.```````````````.`../shddhhhyhhdddddddNMMNNNNMMMMMMMMMMMMMMMMMMMMNy-````````````````````````
-* ``````....````````````..../dmdddmmmNNNNNNNNNNNNNNmNNNNNNNNMMMMNNMMMNNNNNNmmmo.``````````````````````
-* ````````.``.`````````....odhdNMNmmmmmdmdmmmmmmNNNNNNNNNNNMNMMMNNNNNNmNNNNNNNNm+.````````````````````
-* ```````..`.`.```````....+hyddhNhyddddmddmmmmmNNNNNNNNNNNNNNNNNNNmmmNNNMMMMMMMMMm/`````````````
-* ````````.```````````.../yhdNdmdhddmmdmmmdmmmmmmmmmmmmNNNNNNNNmmdmNNMMMMMMMMMMMMMMy`````````````
-* ```````````````````...ohhhdddhdddddmmdddmmmmmmmmdmmmNNNmmmmmmdddddmNNNMMMMMMMMMMMMs````````````````
-*/
