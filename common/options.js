8172d80dce3bd2cb19dca19feae9e895c19d450f
Libor Polčák <ipolcak@fit.vutbr.cz>
2022-01-05 14:22:00 +0100

    Make it possible to export/import configuration files
    This closes #159 on Github

--- common/options.js
+++ common/options.js
@@ -436,24 +436,3 @@ function control_http_request_shield()
 	}
 
 }
-
-////////////////////////////////////////////////////////////////////////////////
-// Debugging
-function load_config_to_text() {
-	browser.storage.sync.get(null).then(function (item) {
-		document.getElementById("levels-storage-text").value = JSON.stringify(item, null, '\t');
-	});
-}
-
-document.getElementById("logoimg").addEventListener("dblclick", function() {
-	document.getElementById("sect-devel").style.display = "block";
-	load_config_to_text();
-});
-
-document.getElementById("levels-storage-load").addEventListener("click", function() {
-	load_config_to_text();
-});
-
-document.getElementById("levels-storage-save").addEventListener("click", function() {
-	browser.storage.sync.set(JSON.parse(document.getElementById("levels-storage-text").value));
-});
