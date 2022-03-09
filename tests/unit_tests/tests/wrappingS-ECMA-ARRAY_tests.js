ea9e231f5b0be3443ac91d8d5f74c935bdcbc9de
martinbednar <martin.bedn20@gmail.com>
2022-03-09 19:42:24 +0100

    Unit tests: Add tests for ECMA-ARRAYS and ECMA-SHARED.
    Add basic unit tests for ECMA-ARRAY and ECMA-SHARED.
    Tests check values and function that should be defined.
    ECMA-ARRAYS tests are being created because there is a known bug in the wrapper. ECMA-ARRAY tests should catch this bug in the future.
    UPDATE: Integrations tests are much more reliable for catching this bug. Use integration tests to test ECMA-ARRAYS, not unit tests.
    
    Known bug: The function window.crypto.getRandomValues(array) expects an array object as a argument. But proxy array object is given by wrapper. This function raises error because of it.

---
+++ tests/unit_tests/tests/wrappingS-ECMA-ARRAY_tests.js
@@ -0,0 +1,120 @@
+//
+//  JavaScript Restrictor is a browser extension which increases level
+//  of security, anonymity and privacy of the user while browsing the
+//  internet.
+//
+//  Copyright (C) 2022 Martin Bednar
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without ev1267027en the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/// <reference path="../../common/wrappingS-ECMA-ARRAY.js">
+
+describe("ECMA-ARRAY", function() {
+	describe("packIEEE754", function() {
+		it("should be defined.",function() {
+			expect(packIEEE754).toBeDefined();
+		});
+		it("should return array.",function() {
+			expect(packIEEE754(14156,2,5)).toEqual(jasmine.any(Array));
+		});
+	});
+	describe("unpackIEEE754", function() {
+		it("should be defined.",function() {
+			expect(unpackIEEE754).toBeDefined();
+		});
+	});
+	describe("unpackF64", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("packF64", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("unpackF32", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("packF32", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("constructDecorator", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("offsetDecorator", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("redefineNewArrayFunctions", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("redefineNewArrayConstructors", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("proxyHandler", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("getByteDecorator", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("setByteDecorator", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("getFloatDecorator", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("setFloatDecorator", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("getBigIntDecorator", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("setBigIntDecorator", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+	describe("redefineDataViewFunctions", function() {
+		it("should be defined.",function() {
+			expect(unpackF64).toBeDefined();
+		});
+	});
+});
