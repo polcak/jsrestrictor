8c1c8008583d2d0335f57bab644a5753f7a5d383
martinbednar <martin.bedn20@gmail.com>
2022-03-09 10:48:52 +0100

    Integration tests: Add the time accuracy test.
    This test discovered a bug in wrapping of Date.getMilliseconds().
    This bug that is probably caused by the commit: https://pagure.io/JShelter/webextension/c/379163dbe40c639aad293214aacac1c2e8ca3769?branch=main
    
    In this commit the NSCL library has been updated.
    
    Bug reproduction:
    
    Web-browser: Google Chrome 98.0.4758.82 (Official Build) (64-bit)
    
    JSR version: 0.6.5 (currently released version: https://chrome.google.com/webstore/detail/javascript-restrictor/ammoloihpcbognfddfjcljgembpibcmb )
    
    JSR - level: 2
    
    Steps:
    1) Open webpage - e.g.: https://polcak.github.io/jsrestrictor/test/test.html
    2) Write to the Google Chrome Developer console: let d = new Date(); d.getMilliseconds();
    Expected return value: number rounded to the hundreds
    Actual return value: unrounded number

--- tests/integration_tests/testing/tests_definition/test_time.py
+++ tests/integration_tests/testing/tests_definition/test_time.py
@@ -3,7 +3,7 @@
 #  of security, anonymity and privacy of the user while browsing the
 #  internet.
 #
-#  Copyright (C) 2020  Martin Bednar
+#  Copyright (C) 2022  Martin Bednar
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
@@ -47,5 +47,28 @@ def test_hours_minutes_seconds(browser):
     p_now = datetime.now()
     p_time = p_now.hour * 60 * 60 + p_now.minute * 60 + p_now.second
     # Values do not have to be strictly equal.
-    # A deviation of less than 4 is tolerated.
-    assert abs(js_time - p_time) < 4
+    # A deviation of less than or equal 1 is tolerated.
+    assert abs(js_time - p_time) <= 1
+
+
+## Test time accuracy.
+def test_accuracy(browser, expected):
+    #Suppose time is rounded.
+    is_time_rounded = True
+    # Make 3 measurement.
+    for _ in range(3):
+        # Wait a while to value of time will be changed.
+        time.sleep(random.randint(1, 3))
+        miliseconds = browser.driver.execute_script("let d = new Date();"
+                                                    "return d.getMilliseconds()")
+        if expected.time['accuracy'] == 'EXACTLY':
+            if int(miliseconds / 10) * 10 != miliseconds:
+                # miliseconds was not rounded. At least one of three measurement has to say value was not rounded.
+                is_time_rounded = False
+        else:
+            assert is_in_accuracy(miliseconds, expected.time['accuracy'])
+ 
+    if expected.time['accuracy'] == 'EXACTLY':
+        # At least one of three measurement has to say value was not rounded.
+        # is_time_rounded should be false if EXACTLY value is required.
+        assert not is_time_rounded
