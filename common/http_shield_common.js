68a656b7cebb8ad92497ce03d9168561b1fb92e2
Libor Polčák <ipolcak@fit.vutbr.cz>
2022-05-13 15:10:30 +0200

    Do not show NBS notifications when accessing 0.0.0.0 or [::]
    Windows machines block these requests.
    Linux machines treat 0.0.0.0/:: as localhost. NBS blocks such requests
    but it takes these as a hint that such blocking was intentional. For
    example, DNS-based blockers can be configured to return the undefined IP
    address for the blocked domain names.

--- common/http_shield_common.js
+++ common/http_shield_common.js
@@ -490,7 +490,7 @@ function isNbsWhitelisted(hostname)
  * \param tabId Tab ID of blocked request.
  */
 function notifyBlockedRequest(origin, target, tabId) {
-	if (nbsSettings.notifications) {
+	if (nbsSettings.notifications && (target !== "0.0.0.0" && target != "[::]")) {
 		nbsNotifications[tabId] = nbsNotifications[tabId] || {};
 		nbsNotifications[tabId].records = nbsNotifications[tabId].records || {};
 		nbsNotifications[tabId].records[`${origin},${target}`] = (nbsNotifications[tabId].records[`${origin},${target}`] || 0) + 1;
