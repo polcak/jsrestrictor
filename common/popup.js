f88f7e1eb8c455ef24ee19e90a9448e157429756
Libor Polčák <ipolcak@fit.vutbr.cz>
2023-03-29 13:18:53 +0200

    Fix 028c7568255a70c72fd2cbbc761f52814e35ce08
    028c7568255a70c72fd2cbbc761f52814e35ce08 supposedly tried to disallow
    tweaks to L0 but instead only allowed tweaking levels whose id can be
    parsed to an integer other than 0.
    
    See https://pagure.io/JShelter/webextension/issue/89 for more
    information.

--- common/popup.js
+++ common/popup.js
@@ -248,7 +248,7 @@ function update_tweaks() {
 		create_tweaks_html(tweaksContainer);
 		tweakBtn.disabled = true;
 	}
-	else if (parseInt(current_level.level_id)) {
+	else if (current_level.level_id !== "0") {
 		tweakBtn.disabled = false;
 		tweakBtn.onclick = function() {
 			create_tweaks_html(tweaksContainer);
