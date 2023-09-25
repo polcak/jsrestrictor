c2290eb846cd1abd9bb18d9ad4c6d39b0ee1a37c
Libor Polčák <polcak@fit.vutbr.cz>
2023-09-25 14:16:54 +0200

    FPD: Remove unnecessary creation of existing array args
    The array already exists in the args variable so this only slowed-down
    the code. I am not sure why this was a part of the code. It was
    introduced by Marek in 376a5a1 (at that time with an uglier code).

--- common/code_builders.js
+++ common/code_builders.js
@@ -71,7 +71,6 @@ function define_page_context_function(wrapper) {
 	var fp_call_count = 0;
 	let replacementF = function(${wrapper.wrapping_function_args}) {
 		try {
-			let args = Array.prototype.slice.apply(arguments);
 			${create_counter_call(wrapper, "call")}
 		}
 		catch (e) { /* No action: let the wrapper continue uninterupted. TODO: let the user decide? */ }`
