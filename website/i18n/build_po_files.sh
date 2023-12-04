2adfaef20dac76e40dd897d2c26347d52da427bd
Ricardo Lafuente <r@manufacturaindependente.org>
2023-12-04 16:46:58 +0000

    Add website PO file generator

---
+++ website/i18n/build_po_files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for f in ../content/pages/*.md; do
+  md2po --quiet \
+    --po-encoding=UTF-8 $f --save --merge-po-files --po-filepath pages.po \
+    -d "Content-Type: text/plain; charset=utf-8" 
+  # -d ”Language: es”
+done
+
+for f in ../content/posts/*.md; do
+  md2po --quiet \
+    --po-encoding=UTF-8 $f --save --merge-po-files --po-filepath posts.po \
+    -d "Content-Type: text/plain; charset=utf-8" 
+  # -d ”Language: es”
+done
+
+for f in ../content/wrappers/*.md; do
+  md2po --quiet \
+    --po-encoding=UTF-8 $f --save --merge-po-files --po-filepath wrappers.po \
+    -d "Content-Type: text/plain; charset=utf-8" 
+  # -d ”Language: es”
+done
